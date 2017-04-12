5de1a940bcc57d271f2b349552edde5ac95f7525
Jalal ul Deen <jalal.din@gmail.com>
2017-04-12 16:09:05 +0100

    Draw canvas boundr's horizontal lines

--- src/test/java/com/jalalsoft/shapes/canvas/DrawBoundaryLinesTest.java
+++ src/test/java/com/jalalsoft/shapes/canvas/DrawBoundaryLinesTest.java
@@ -37,5 +37,28 @@ public class DrawBoundaryLinesTest {
                         "       \n", canvas.toString());
 
     }
+    @Test
+    public void whenYCoordinatesAreSame_thenDrawHorizontalLine() {
 
+        int x1 = 0, y1 = 0;
+        int x2 = 6, y2 = 0;
+        char lineChar = '-';
+
+        Line line = new Line(x1, y1, x2, y2);
+
+        int width = 5;
+        int height = 1;
+        Canvas canvas = null;
+        canvas = new Canvas(width, height);
+        LineMarker printer = new LineMarker();
+
+        printer.mark(line, canvas, lineChar);
+
+        assertEquals("Line isn't printed correctly",
+                "-------\n" +
+                        "       \n" +
+                        "       \n", canvas.toString());
+
+
+    }
 }
