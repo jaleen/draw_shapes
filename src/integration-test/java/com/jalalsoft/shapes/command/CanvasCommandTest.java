2dad64dc18cd21ed9568088f5ee5ef3163b2273f
Jalal ul Deen <jalal.din@gmail.com>
2017-04-12 15:04:15 +0100

    Draw a fixed canvas

---
+++ src/integration-test/java/com/jalalsoft/shapes/command/CanvasCommandTest.java
@@ -0,0 +1,30 @@
+package com.jalalsoft.shapes.command;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by jalal.deen on 12/04/2017.
+ */
+public class CanvasCommandTest {
+
+    @Test
+    public void givenCanvasCommand_whenWidthHeightProvided_theDrawCanvas(){
+
+        String canvasUserInput = "5 5";
+        Command command = new CreateCanvasCommand();
+
+        String drawing = command.execute(canvasUserInput);
+
+        String expectedDrawing = "-------\n" +
+                "|     |\n" +
+                "|     |\n" +
+                "|     |\n" +
+                "|     |\n" +
+                "|     |\n" +
+                "-------\n";
+        assertEquals("Canvas isn't drawn correctly", expectedDrawing, drawing);
+
+    }
+}
